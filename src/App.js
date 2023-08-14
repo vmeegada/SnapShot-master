9ecff643b5e90bea47caad46c2e5d720c44d4924
vmeegada <vasavi.anu98@gmail.com>
2023-08-14 12:25:44 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PhotoContextProvider from "./context/PhotoContext";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Item from "./components/Item";
 import Search from "./components/Search";
@@ -18,41 +18,19 @@ class App extends Component {
   render() {
     return (
       <PhotoContextProvider>
-        <HashRouter basename="/SnapScout">
-          <div className="container">
-            <Route
-              render={props => (
-                <Header
-                  handleSubmit={this.handleSubmit}
-                  history={props.history}
-                />
-              )}
-            />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => <Redirect to="/mountain" />}
-              />
-
-              <Route
-                path="/mountain"
-                render={() => <Item searchTerm="mountain" />}
-              />
-              <Route path="/beach" render={() => <Item searchTerm="beach" />} />
-              <Route path="/bird" render={() => <Item searchTerm="bird" />} />
-              <Route path="/food" render={() => <Item searchTerm="food" />} />
-              <Route
-                path="/search/:searchInput"
-                render={props => (
-                  <Search searchTerm={props.match.params.searchInput} />
-                )}
-              />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </HashRouter>
-      </PhotoContextProvider>
+      <div className="container">
+        <Header handleSubmit={this.handleSubmit} />
+        <Routes>
+          <Route path="/" element={<Navigate to="/mountain" />} />
+          <Route path="/mountain" element={<Item searchTerm="mountain" />} />
+          <Route path="/beach" element={<Item searchTerm="beach" />} />
+          <Route path="/bird" element={<Item searchTerm="bird" />} />
+          <Route path="/food" element={<Item searchTerm="food" />} />
+          <Route path="/search/:searchInput" element={<Search />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </div>
+    </PhotoContextProvider>
     );
   }
 }
